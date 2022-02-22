7d6270f189550deadda0abd9f1397f10b35c0a4a
Jen Truong <jennifertruong09@gmail.com>
2022-02-21 20:09:02 -0500

    added layout to the seeds.sql and got rid of not null from 2 values in the schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,10 @@
 -- to pre-populate the database 
 -- reference seeds.sql from activity 12-12
+INSERT INTO department (department_name)
+VALUES (Engineering), (Finance), (Legal), (Sales)
+
+INSERT INTO department_role (id, title, departmenet_id, salary)
+VALUES 
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
